a5a238884d4c0aa14e8a3a39166169792f33062a
VitaliyVolchenko <vitaliyvolchenkodeveloper@gmail.com>
2018-06-19 17:11:10 +0300

    Added Auth

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'login', 'email', 'password',
     ];
 
     /**
@@ -26,4 +26,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+   
 }
